e0baf4f8e4192c61fa58b7486bb75499eeca1968
Aidan Connolly <aidan@colts.org>
2020-01-30 20:35:40 -0600

    Adding recaptcha service to whistleblower form

--- whistleblowerscript.php
+++ whistleblowerscript.php
@@ -1,24 +1,5 @@
 <?php
 
-if(isset($_POST['g-recaptcha-response'])) {
-   // RECAPTCHA SETTINGS
-   $captcha = $_POST['g-recaptcha-response'];
-   $ip = $_SERVER['REMOTE_ADDR'];
-   $key = '6LdPsW8UAAAAAAZ_MTtJlqxESaBwaQb5j7YVpRa_';
-   $url = 'https://www.google.com/recaptcha/api/siteverify';
- 
-   // RECAPTCH RESPONSE
-   $recaptcha_response = file_get_contents($url.'?secret='.$key.'&response='.$captcha.'&remoteip='.$ip);
-   $data = json_decode($recaptcha_response);
- 
-   if(isset($data->success) &&  $data->success === true) {
-       // code goes here
-   }
-   else {
-      die('Please click "back" and be sure to check the reCAPTCHA box before submitting. Thank you!');
-   }
-}
-
 $message=$_POST['message'];
 
 $to="whistleblower@colts.org, jeff@colts.org, petermh@msn.com, jafjohns@indiana.edu, wlhowes@mchsi.com, bsmith@yousq.net, colts.cooks@comcast.net, cathiroberts.omaha@gmail.com, coltswhistleblower@gmail.com";
