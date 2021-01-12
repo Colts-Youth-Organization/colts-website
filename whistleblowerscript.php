4e6d59d23ce64abbf8a5ead29ade4fa77ba2aa17
Aidan Connolly <aidan@colts.org>
2021-01-12 11:25:40 -0600

    Updating executive board list for whistleblower

--- whistleblowerscript.php
+++ whistleblowerscript.php
@@ -6,7 +6,7 @@ $email=$_POST['email'];
 if ($email != "") {
     readfile("whistleblower_error.html");
 } else {
-    $to="whistleblower@colts.org, jeff@colts.org, petermh@msn.com, jafjohns@indiana.edu, wlhowes@mchsi.com, bsmith@yousq.net, colts.cooks@comcast.net, cathiroberts.omaha@gmail.com, coltswhistleblower@gmail.com, aidan@colts.org";
+    $to="whistleblower@colts.org, jeff@colts.org, jafjohns@indiana.edu, wlhowes@mchsi.com, bsmith@yousq.net, mpkelley@gmail.com, eelmshaeuser@gmail.com, cathiroberts.omaha@gmail.com, coltswhistleblower@gmail.com, aidan@colts.org";
 
     $subject="Whistleblower Policy Submission";
     $body="MESSAGE - $message\n";
