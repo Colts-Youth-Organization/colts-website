ad86e423ace8564683b235252f923db58394d04d
aidancolts <aidan@colts.org>
2021-03-12 16:04:42 -0600

    End March 12

---
+++ whistleblowerscript.php
@@ -0,0 +1,18 @@
+<?php
+
+$message=$_POST['message'];
+$email=$_POST['email'];
+
+if ($email != "") {
+    readfile("whistleblower_error.html");
+} else {
+    $to="whistleblower@colts.org, jeff@colts.org, jafjohns@indiana.edu, wlhowes@mchsi.com, bsmith@yousq.net, mpkelley@gmail.com, eelmshaeuser@gmail.com, cathiroberts.omaha@gmail.com, coltswhistleblower@gmail.com, aidan@colts.org";
+
+    $subject="Whistleblower Policy Submission";
+    $body="MESSAGE - $message\n";
+
+    mail ( $to, $subject, $body );
+
+    readfile("whistleblower_submission.html");
+}
+?>
