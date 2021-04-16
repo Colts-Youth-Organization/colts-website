72e36d21981de51359260b33ee69e741553ffef3
aidancolts <aidan@colts.org>
2021-04-16 14:30:25 -0500

    accommodation

---
+++ accommodation-form.php
@@ -0,0 +1,16 @@
+<?php
+
+$name=$_POST['name'];
+$email=$_POST['email'];
+$corps=$_POST['corps'];
+$message=$_POST['message'];
+
+$to="aidan@colts.org";
+
+$subject="Accommodation Request Submission";
+$body="Name: $name\Email: $email\nCorps, group or event: $corps\nMessage: $message\n";
+
+mail ( $to, $subject, $body );
+
+readfile("accommodation-form-submission.html");
+?>
